380964e1fa31cf3db068907afb51696afcf5453b
Matheus de Assis Alcântara <matheus.labsit@dotzmkt.com.br>
2022-08-05 23:52:02 -0300

    t

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/myapp'));
+app.use(express.static('./dist/coreui-free-angular-admin-template'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/app-heroku/'}),
